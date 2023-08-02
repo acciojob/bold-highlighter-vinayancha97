0c30568726bc7e4f9c816266230522305c296717
vinayancha97 <135926111+vinayancha97@users.noreply.github.com>
2023-08-02 22:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function highlight() {
     //Write your code here
-
-
+	var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'green';
+  });
 }
-
-
 function return_normal() {
     //Write your code here
-
-    
+	var boldWords = document.querySelectorAll('strong')
+  boldWords.forEach(function(word) {
+    word.style.color = 'black';
+  });  
 }
